885a99939e85cf0529ebcb7dd196cafba7f516f4
Varneon <me@varneon.com>
2022-11-13 04:27:34 -0800

    Initial file commit

---
+++ Packages/com.varneon.vudon.layermask-dropdown/Editor/LayerMaskDropdownBuildPostProcessor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UdonSharpEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.Events;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using VRC.Udon;
+using static UnityEngine.EventSystems.EventTrigger;
+
+namespace Varneon.VUdon.LayerMaskDropdown.Editor
+{
+    internal static class LayerMaskDropdownPostProcessor
+    {
+        [PostProcessScene(-1)] // Ensure that all of the dropdowns get processed before U# saves the C# component data into UdonBehaviours
+        private static void InitializeLayerMaskDropdowns()
+        {
+            // Find all LayerMaskDropdowns and setup all of them
+            foreach(LayerMaskDropdown dropdown in UnityEngine.Object.FindObjectsOfType<LayerMaskDropdown>())
+            {
+                SetupLayerMaskDropdown(dropdown);
+            }
+        }
+
+        /// <summary>
+        /// Sets up a LayerMaskDropdown to match the project's current settings and links the required components 
+        /// </summary>
+        /// <param name="layerMaskDropdown"></param>
+        private static void SetupLayerMaskDropdown(LayerMaskDropdown layerMaskDropdown)
+        {
+            List<int> layerIndices = new List<int>();
+
+            List<string> optionNames = new List<string>() { "Nothing", "Everything" };
+
+            for (int i = 0; i < 32; i++)
+            {
+                string name = LayerMask.LayerToName(i);
+
+                bool isDefined = !string.IsNullOrEmpty(name);
+
+                if (isDefined)
+                {
+                    optionNames.Add(string.Format("{0}: {1}", i, name));
+
+                    layerIndices.Add(i);
+                }
+            }
+
+            Dropdown dropdown = layerMaskDropdown.GetComponent<Dropdown>();
+
+            FieldInfo layerCountField = typeof(LayerMaskDropdown).GetField("projectLayerCount", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            FieldInfo layersField = typeof(LayerMaskDropdown).GetField("layerIndices", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            EventTrigger trigger = layerMaskDropdown.GetComponent<EventTrigger>();
+
+            layerCountField.SetValue(layerMaskDropdown, optionNames.Count - 2);
+
+            layersField.SetValue(layerMaskDropdown, layerIndices.ToArray());
+
+            dropdown.options = optionNames.Select(l => new Dropdown.OptionData(l)).ToList();
+
+            UdonBehaviour ub = UdonSharpEditorUtility.GetBackingUdonBehaviour(layerMaskDropdown);
+
+            MethodInfo sendCustomEventInfo = UnityEventBase.GetValidMethodInfo(ub, nameof(UdonBehaviour.SendCustomEvent), new[] { typeof(string) });
+
+            UnityAction<string> sendCustomEventDelegate = Delegate.CreateDelegate(typeof(UnityAction<string>), ub, sendCustomEventInfo, false) as UnityAction<string>;
+
+            UnityEventTools.AddStringPersistentListener(dropdown.onValueChanged, sendCustomEventDelegate, "OnValueChanged");
+
+            Entry dropdownClickEntry = new Entry
+            {
+                eventID = EventTriggerType.PointerClick,
+                callback = new TriggerEvent()
+            };
+            UnityEventTools.AddStringPersistentListener(dropdownClickEntry.callback, sendCustomEventDelegate, "OnClick");
+
+            Toggle templateItem = dropdown.template.GetComponentInChildren<Toggle>();
+
+            EventTrigger templateItemTrigger = templateItem.GetComponent<EventTrigger>() ?? templateItem.gameObject.AddComponent<EventTrigger>();
+
+            Entry itemClickEntry = new Entry
+            {
+                eventID = EventTriggerType.PointerClick,
+                callback = new TriggerEvent()
+            };
+            UnityEventTools.AddStringPersistentListener(itemClickEntry.callback, sendCustomEventDelegate, "OnItemClick");
+
+            trigger.triggers.Add(dropdownClickEntry);
+            templateItemTrigger.triggers.Add(itemClickEntry);
+        }
+    }
+}
---
+++ Packages/com.varneon.vudon.layermask-dropdown/Runtime/Udon Programs/LayerMaskDropdown.cs
@@ -0,0 +1,188 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using VRC.Udon;
+
+namespace Varneon.VUdon.LayerMaskDropdown
+{
+    [RequireComponent(typeof(Dropdown))]
+    [RequireComponent(typeof(EventTrigger))]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LayerMaskDropdown : UdonSharpBehaviour
+    {
+        /// <summary>
+        /// Current LayerMask
+        /// </summary>
+        [SerializeField]
+        private LayerMask mask;
+
+        /// <summary>
+        /// Target UdonBehaviour for applying the LayerMask value
+        /// </summary>
+        [SerializeField]
+        private UdonBehaviour target;
+
+        /// <summary>
+        /// Name of the target UdonBehaviour's LayerMask variable
+        /// </summary>
+        [SerializeField]
+        private string variable;
+
+        /// <summary>
+        /// Number of layers defined in this project
+        /// </summary>
+        [SerializeField, HideInInspector]
+        private int projectLayerCount;
+
+        /// <summary>
+        /// Indices of all of the defined layers in the project
+        /// </summary>
+        [SerializeField, HideInInspector]
+        private int[] layerIndices;
+
+        private Dropdown dropdown;
+
+        private int maskInt;
+
+        private Toggle[] cullingMaskToggles;
+
+        private Text dropdownValueLabel;
+
+        private bool isOpen;
+
+        private bool selectionQueued;
+
+        private void Start()
+        {
+            dropdown = GetComponent<Dropdown>();
+
+            dropdownValueLabel = dropdown.captionText;
+
+            maskInt = mask;
+
+            SetTargetVariable();
+
+            CheckMixedLayers();
+        }
+
+        private void SetTargetVariable()
+        {
+            if(target != null && !string.IsNullOrWhiteSpace(variable))
+            {
+                target.SetProgramVariable(variable, mask);
+            }
+        }
+
+        private void OpenCullingMaskMenu()
+        {
+            isOpen = true;
+
+            cullingMaskToggles = dropdown.GetComponentsInChildren<Toggle>(true);
+
+            for (int i = 0; i < projectLayerCount; i++)
+            {
+                cullingMaskToggles[i + 4].SetIsOnWithoutNotify((maskInt & (1 << layerIndices[i])) != 0);
+            }
+        }
+
+        public void OnClick()
+        {
+            OpenCullingMaskMenu();
+        }
+
+        public void OnItemClick()
+        {
+            selectionQueued = true;
+
+            SendCustomEventDelayedFrames(nameof(PostProcessItemClick), 0);
+        }
+
+        public void PostProcessItemClick()
+        {
+            if (!selectionQueued || !isOpen) { return; }
+
+            UpdateCullingMask();
+        }
+
+        private void UpdateCullingMask()
+        {
+            int selectedLayerOption = dropdown.value;
+
+            switch (selectedLayerOption)
+            {
+                case 0: // Nothing
+                    maskInt = 0;
+                    break;
+                case 1: // Everything
+                    maskInt = -1;
+                    break;
+                default:
+                    int mask = 1 << layerIndices[selectedLayerOption - 2]; // Create bitmask of the layer that was clicked
+
+                    if (isOpen)
+                    {
+                        if ((maskInt & mask) != 0) // If same bit is set on both the culling mask and the selection mask
+                        {
+                            maskInt &= ~mask; // Reset the bit
+                        }
+                        else
+                        {
+                            maskInt |= mask; // Set the bit
+                        }
+                    }
+
+                    CheckMixedLayers();
+                    break;
+            }
+
+            mask = maskInt;
+
+            SetTargetVariable();
+
+            selectionQueued = false;
+
+            isOpen = false;
+        }
+
+        private void CheckMixedLayers()
+        {
+            int layerCount = 0;
+
+            int optionIndex = 0;
+
+            for (int i = 0; i < projectLayerCount; i++)
+            {
+                if ((maskInt & (1 << layerIndices[i])) != 0)
+                {
+                    optionIndex = i;
+
+                    layerCount += 1;
+                }
+            }
+
+            Debug.Log($"{name}\n{layerCount}\n{optionIndex}");
+
+            if (layerCount == projectLayerCount)
+            {
+                maskInt = -1;
+
+                dropdown.SetValueWithoutNotify(1);
+            }
+            else if (layerCount > 1) // If multiple layers are selected, set the preview label as "Mixed..."
+            {
+                dropdown.SetValueWithoutNotify(optionIndex + 2);
+
+                dropdownValueLabel.text = "Mixed...";
+            }
+            else if (layerCount == 1) // If only one layer is selected, override the dropdown with the remaining layer
+            {
+                dropdown.SetValueWithoutNotify(optionIndex + 2);
+            }
+            else if (layerCount == 0) // If no layers have been selected, override the dropdown to Nothing / 0
+            {
+                dropdown.SetValueWithoutNotify(0);
+            }
+        }
+    }
+}
